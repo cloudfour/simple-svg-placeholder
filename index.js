b09a9298413249428a79b1bdd5f483cb55440573
Tyler Sticka <tyler@tylersticka.com>
2020-01-03 10:31:01 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+export default function simpleSvgPlaceholder({
+  width,
+  height = width,
+  text = `${width}&#215;${height}`,
+  fontFamily = 'sans-serif',
+  fontWeight = 'bold',
+  fontSize = Math.floor(Math.min(width, height) * 0.2),
+  textAdjust = Math.floor(fontSize * 0.4),
+  bgColor = '#ddd',
+  textColor = 'rgba(0,0,0,0.5)',
+  dataUri = true
+} = {}) {
+  const str = `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}">
+    <rect fill="${bgColor}" width="${width}" height="${height}"/>
+    <text fill="${textColor}" font-family="${fontFamily}" font-size="${fontSize}" dy="${textAdjust}" font-weight="${fontWeight}" x="50%" y="50%" text-anchor="middle">${text}</text>
+  </svg>`;
+
+  // Thanks to: filamentgroup/directory-encoder
+  const cleaned = str
+    .replace(/[\n\r]/gim, '') // Strip newlines
+    .replace(/\t/gim, ' ') // Replace tabs with strings
+    .replace(/'/gim, '\\i'); // Normalize quotes
+
+  if (dataUri) {
+    const encoded = encodeURIComponent(cleaned)
+      .replace(/\(/g, '%28') // Encode brackets
+      .replace(/\)/g, '%29');
+
+    return 'data:image/svg+xml;charset=US-ASCII,' + encoded;
+  }
+
+  return cleaned;
+}
